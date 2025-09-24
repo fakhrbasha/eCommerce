cce824b53dd01393a5ecabc7ed4f4a9a8e33918d
fakhrbasha <fakhrbasha8@gmail.com>
2025-09-24 20:26:40 +0300

    LAST

--- src/services/api.ts
+++ src/services/api.ts
@@ -34,7 +34,7 @@ class ApiServices {
 
     constructor() {
         this.#baseURL =
-            process.env.NEXT_PUBLIC_API_URL || "https://ecommerce.routemisr.com";
+            process.env.NEXT_PUBLIC_API_BASE_URL || "https://ecommerce.routemisr.com";
     }
 
     // dynamic headers
